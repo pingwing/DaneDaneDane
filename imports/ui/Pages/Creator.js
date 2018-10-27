affc05d6184a84d0b903764f29f21a0cd807e4ba
archiewald <artur.kozubek1@gmail.com>
2018-10-27 16:21:22 +0200

    💩

--- imports/ui/Pages/Creator.js
+++ imports/ui/Pages/Creator.js
@@ -3,6 +3,6 @@ import { Page } from "../Layout/Page";
 
 export default class Creator extends Component {
   render() {
-    return <Page footer={<div>Footer</div>}>Template</Page>;
+    return <Page>Template</Page>;
   }
 }
