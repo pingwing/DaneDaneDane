f97097ab8f9708d110cfeafb57e49b4472aebad5
archiewald <artur.kozubek1@gmail.com>
2018-10-27 17:11:17 +0200

    add dropzone

--- imports/ui/Pages/FileUpload.js
+++ imports/ui/Pages/FileUpload.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import Dropzone from "react-dropzone";
+
 import { Meteor } from "meteor/meteor";
 
 export default class FileUpload extends Component {
   render() {
-    return <div>tutaj</div>;
+    return (
+      <div>
+        <Dropzone
+          ref={el => (this.dropzone = el)}
+          onDropAccepted={this.onDropAccepted}
+          multiple={false}
+        />
+        <button onClick={this.onButtonClick}>Załącz plik</button>
+      </div>
+    );
   }
+
+  onDropAccepted = files => {
+    console.log(files);
+  };
+
+  onButtonClick = () => {
+    this.dropzone.open();
+  };
 }
