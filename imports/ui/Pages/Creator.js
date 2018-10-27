6c6bf98d60367a62aca8728095bdd8938ddff0ed
archiewald <artur.kozubek1@gmail.com>
2018-10-27 23:14:09 +0200

    progress on creator

--- imports/ui/Pages/Creator.js
+++ imports/ui/Pages/Creator.js
@@ -1,8 +1,53 @@
 import React, { Component } from "react";
 import { Page } from "../Layout/Page";
+import DataType from "./DataType";
+import Levels from "./Levels";
+
+import StepZilla from "react-stepzilla";
+import "./StepZilla.css";
+import "react-sortable-tree/style.css";
 
 export default class Creator extends Component {
-  render() {
-    return <Page>Template</Page>;
+  constructor(props = {}) {
+    super(props);
+    this.state = {
+      category: "",
+      dataType: "",
+      levelsNumber: undefined
+    };
   }
+
+  render() {
+    const steps = [
+      {
+        name: "Nazwa i rodzaj",
+        component: (
+          <DataType
+            value={this.state.category}
+            setCategory={this.onSetCategory}
+          />
+        )
+      },
+      { name: "Poziomy", component: <Levels /> },
+      { name: "Values", component: <DataType /> },
+      { name: "Podgląd", component: <DataType /> },
+      { name: "Typ podkategorii", component: <DataType /> },
+      { name: "Pobierz plik", component: <DataType /> }
+    ];
+
+    return (
+      <Page>
+        <div className="step-progress">
+          <StepZilla steps={steps} />
+        </div>
+      </Page>
+    );
+  }
+
+  onSetCategory = event => {
+    this.setState({
+      ...this.state,
+      category: event.target.value
+    });
+  };
 }
