5c1ea6e24235a470ad120f32b1a86e257ded0f27
archiewald <artur.kozubek1@gmail.com>
2018-10-27 23:41:54 +0200

    add value component

--- imports/ui/Pages/Creator.js
+++ imports/ui/Pages/Creator.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import { Page } from "../Layout/Page";
 import DataType from "./DataType";
-import Levels from "./Levels";
+import Value from "./Value";
 
 import StepZilla from "react-stepzilla";
 import "./StepZilla.css";
 import "react-sortable-tree/style.css";
 
+// const categories = [{ name: "", values: [] }];
+
+// const values = [{ name: "", dataType: "" }];
+
 export default class Creator extends Component {
   constructor(props = {}) {
     super(props);
     this.state = {
-      category: "",
+      categories: [],
+      value: {
+        type: "",
+        value: ""
+      },
       dataType: "",
       levelsNumber: undefined
     };
@@ -20,16 +28,16 @@ export default class Creator extends Component {
   render() {
     const steps = [
       {
-        name: "Nazwa i rodzaj",
+        name: "Wartości",
         component: (
-          <DataType
-            value={this.state.category}
-            setCategory={this.onSetCategory}
+          <Value
+            value={this.state.value}
+            setValueType={this.onSetValueType}
+            setValueName={this.onSetValueName}
           />
         )
       },
-      { name: "Poziomy", component: <Levels /> },
-      { name: "Values", component: <DataType /> },
+      // { name: "Kategorie", component: <Categories /> },
       { name: "Podgląd", component: <DataType /> },
       { name: "Typ podkategorii", component: <DataType /> },
       { name: "Pobierz plik", component: <DataType /> }
@@ -44,10 +52,23 @@ export default class Creator extends Component {
     );
   }
 
-  onSetCategory = event => {
+  onSetValueType = event => {
     this.setState({
       ...this.state,
-      category: event.target.value
+      value: {
+        ...this.state.value,
+        type: event.target.value
+      }
+    });
+  };
+
+  onSetValueName = event => {
+    this.setState({
+      ...this.state,
+      value: {
+        ...this.state.value,
+        name: event.target.value
+      }
     });
   };
 }
