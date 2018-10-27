eb276f5260b3c857acbca4cb4f99c9fdd019ee53
archiewald <artur.kozubek1@gmail.com>
2018-10-27 14:56:55 +0200

    add container

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,6 +4,7 @@ import createHistory from "history/createBrowserHistory";
 import Creator from "/imports/ui/Creator";
 import Main from "/imports/ui/Main";
 import NavBar from "./Layout/NavBar";
+import Typography from "@material-ui/core/Typography";
 
 const history = createHistory();
 
@@ -13,34 +14,36 @@ export default class App extends Component {
       <Router history={history}>
         <div>
           <NavBar />
-          <Switch>
-            <Route path="/" component={Main} exact />
-            <Route
-              path="/about"
-              component={() => (
-                <div>
-                  <h1>Po co dobre dane?</h1>
-                </div>
-              )}
-            />
-            <Route
-              path="/best_practices"
-              component={() => (
-                <div>
-                  <h1>Dobre praktyki</h1>
-                </div>
-              )}
-            />
-            <Route path="/creator" component={Creator} />
-            <Route
-              path="/upload"
-              component={() => (
-                <div>
-                  <h1>Udostępnij dane</h1>
-                </div>
-              )}
-            />
-          </Switch>
+          <Typography component="div" style={{ padding: 8 * 3 }}>
+            <Switch>
+              <Route path="/" component={Main} exact />
+              <Route
+                path="/about"
+                component={() => (
+                  <div>
+                    <h1>Po co dobre dane?</h1>
+                  </div>
+                )}
+              />
+              <Route
+                path="/best_practices"
+                component={() => (
+                  <div>
+                    <h1>Dobre praktyki</h1>
+                  </div>
+                )}
+              />
+              <Route path="/creator" component={Creator} />
+              <Route
+                path="/upload"
+                component={() => (
+                  <div>
+                    <h1>Udostępnij dane</h1>
+                  </div>
+                )}
+              />
+            </Switch>
+          </Typography>
         </div>
       </Router>
     );
